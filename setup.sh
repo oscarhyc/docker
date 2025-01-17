8d3bd39b0f251d0837d18123622e6b47dda8fe30
oscarhyc <150122497+oscarhyc@users.noreply.github.com>
2025-01-17 20:00:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,8 +28,7 @@ newgrp docker
 
 . ./.env
 
-if [ "$DATABASE_TYPE" = "mysql" ] && [ "$LOCAL_DB" = "no" ]; then
-    docker run -v ./backend/db/mysql_init.sh:/mysql_init.sh --env-file .env --rm mysql_init:latest ./mysql_init.sh
-    docker rmi $(docker images 'mysql' -a -q)
+if [ "$DATABASE_TYPE" = "postgres" ] && [ "$LOCAL_DB" = "no" ]; then
+    docker run -v ./backend/db/init.sh:/init.sh --env-file .env --rm postgres:17.0-alpine ./init.sh
     echo "$DATABASE_TYPE" database init completed;
 fi
